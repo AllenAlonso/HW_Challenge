2d0ce16795a5ec3ccdf161746b2a06eaa3371680
AllenAlonso <70241507+AllenAlonso@users.noreply.github.com>
2023-03-13 21:42:50 -0700

    Update Stu_PyP.py

--- Stu_PyP.py
+++ Stu_PyP.py
@@ -8,38 +8,44 @@ import pandas as pd
 import csv
 import os
 
+#path to csv file
 file = "Resources/election_data.csv"
 df = pd.read_csv(file)
 
 
-
+#using pandas to get a sense of all  totals
 total_votes = df["Ballot ID"].count()
-candidates = df["Candidate"].unique()
-
 percentages = df["Candidate"].value_counts()/total_votes *100
 
+#re-frames percentages as its own df, reset index in order to put canditate name as column for iloc
 percentages_df = pd.DataFrame({"Percentage": percentages})
 percentages_df = percentages_df.reset_index()
 
+#gets total votes for each candidate, reset index from greatest to lowest
 votes = pd.DataFrame(df["Candidate"].value_counts())
 votes = votes.reset_index()
 votes = votes.sort_values("Candidate",ascending=False)
 
+#gets max for the organized votes df
 winner = votes.iloc[0,0]
 
+#recording all candidates in specified order into vars
+#could likely instead try to loop to generalize the csv reading, but since we have the format this works
 cand1 = votes.iloc[1,0]
 cand2 = votes.iloc[0,0]
 cand3 = votes.iloc[2,0]
 
+#recording all percentages in specified order into vars
 perc1 = percentages_df.iloc[1,1]
 perc2 = percentages_df.iloc[0,1]
 perc3 = percentages_df.iloc[2,1]
 
+#recording all vote numbers in specified order into vars
 vote1 = votes.iloc[1,1]
 vote2 = votes.iloc[0,1]
 vote3 = votes.iloc[2,1]
 
-
+#printing information
 print("Election Results")
 print("----------------------")
 print(f"Total Votes: {total_votes}")
@@ -51,7 +57,7 @@ print("----------------------")
 print(f"Winner: {winner}")
 print("----------------------")
 
-
+#creating output file
 out = open("Resources/output.txt","w")
 
 x0= "Election Results"
@@ -65,10 +71,10 @@ x7= "----------------------"
 x8= str(f"Winner: {winner}")
 x9= "----------------------"
 
+#writing into the text file with previous x vars
 out.write(f'{x0}\n{x1}\n{x2}\n{x3}\n{x4}\n{x5}\n{x5}\n{x6}\n{x7}\n{x8}\n{x9}\n')
 
 
-# In[ ]:
 
 
 
